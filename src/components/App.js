fc91c16705677d2d1b5cb2c9e8d5bf6cd20f37c8
Nanda-Souza <fesouoli@gmail.com>
2022-11-21 03:32:54 -0300

    Componentes v1

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar"
+import Corpo from "../components/Corpo"
+import FundoMobile from "../components/FundoMobile"
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Corpo />
+      <FundoMobile />      
+    </div>
+  );
+}
